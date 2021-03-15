17bd2e2919b8d155b94e71f82bf9f3937e3d6b59
MOVS Laboratory <lab.movs@outlook.com>
2021-03-15 18:18:49 +0500

    third task

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+prof_list = ['engineer', 'pilot', 'builder', 'doctor', 'biologist']
+
 
 @app.route('/')
 @app.route('/index/<title>')
 def index(title='Mars'):
     return render_template('base.html', title=title)
+
+
+@app.route('/list_prof/<kind>')
+def profession(kind):
+    return render_template('prof_list.html', type=kind, prof_list=prof_list)
 
 
 if __name__ == '__main__':
